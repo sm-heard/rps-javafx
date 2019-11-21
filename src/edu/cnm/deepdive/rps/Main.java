8116c5b7bc7764af74c832335bda9001b3c11bea
Nick Bennett <nick@nickbenn.com>
2019-11-21 12:22:38 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/rps/Main.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.rps;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+
+  }
+  
+}
